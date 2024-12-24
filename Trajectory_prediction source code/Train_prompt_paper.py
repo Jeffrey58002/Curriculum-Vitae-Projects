4a4db7016bbecd4d5ba53c34df868773e82402dd
Jeffrey58002 <136485151+Jeffrey58002@users.noreply.github.com>
2024-12-24 18:44:11 +0800

    Delete Trajectory_prediction source code/Train_prompt_paper.py

--- Trajectory_prediction source code/Train_prompt_paper.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the CSV file to check its content
-data_path = '/mnt/data/shortened_pred.csv'
-data = pd.read_csv(data_path)
-
-# Display the first few rows of the dataset
-data.head(), data.info(), data.describe()
